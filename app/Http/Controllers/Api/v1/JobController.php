f2dbacc3c6ddedf09af050caf6cb78f4b9883db3
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2021-03-08 11:59:58 +0100

    Fix error handling in Job Controller

--- app/Http/Controllers/Api/v1/JobController.php
+++ app/Http/Controllers/Api/v1/JobController.php
@@ -12,14 +12,23 @@ class JobController extends Controller
     {
         $jobs = Job::all();
 
-        return response()->json(['data' => $jobs->toArray()], 200);
+        if (count($jobs) > 1) {
+            return response()->json(['data' => $jobs->toArray()], 200);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'No Jobs Available!'
+        ], 200);
     }
 
     public function store(Request $request)
     {
         $input = $request->all();
         $input['user_id'] = $request->user()->id;
+
         $data = Job::create($input);
+
         return response()->json([
             'status' => 'success',
             'message' => 'Job Successfully Created!', 'data' => $data->toArray()
@@ -28,8 +37,16 @@ class JobController extends Controller
 
     public function show($id)
     {
-        $data = Job::findOrFail($id);
-        return response()->json(['data' => $data->toArray()], 200);
+        $data = Job::find($id);
+
+        if ($data) {
+            return response()->json(['data' => $data->toArray()], 200);
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Job Not Found!'
+        ], 404);
     }
 
     public function delete(Request $request, $id)
