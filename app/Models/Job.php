781505affd3c6f5cd793687de83baff9a44fc5f9
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2021-03-09 11:00:50 +0100

    add submission_deadline column to jobs table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,7 +9,7 @@ class Job extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'title', 'company', 'company_logo', 'location', 'salary', 'description', 'benefits', 'type', 'condition'];
+    protected $fillable = ['user_id', 'title', 'company', 'company_logo', 'location', 'salary', 'description', 'benefits', 'type', 'condition', 'submission_deadline'];
 
     /**
      * Get the applications for the job.
